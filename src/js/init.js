1bfa511c0fd7f6566ee529d07d3d9618caef5e2f
Іванчик Антон Петрович <ivanchikap@nsj.gov.ua>
2021-11-16 16:10:56 +0200

    less8 part1

--- src/js/init.js
+++ src/js/init.js
@@ -2,4 +2,11 @@ $(function() {
     $("#services-tabs").tabs();
 
     $("#portfolio__tabs").tabs();
+
+    $('#owl-carousel').owlCarousel({
+        loop:true,
+        items:1,
+        nav: true,
+        dots: true
+    })
 });
